4933c2676d648f33af64829a96f6202d2f50c6e5
Bastinanub <bastinanub9@gmail.com>
2024-06-05 16:16:31 -0400

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,56 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country = models.TextField()
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(1800)
+        ]
+    )
+    def __str__(self):
+        return self.name
+
+class CarModel(models.Model):
+    CAR_TYPES = [
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
+    ]
+
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default="SUV"
+    )
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+    price = models.IntegerField(
+        default=10000,
+        validators=[
+            MaxValueValidator(999999999),
+            MinValueValidator(1)
+        ]
+    )
+
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
